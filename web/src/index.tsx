1e4390b3c3981c697dc4d4d7b1ffec2138d81b54
Charles Yu <charles.yu108@gmail.com>
2024-06-27 09:52:26 -0700

    Add site stats

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
 
